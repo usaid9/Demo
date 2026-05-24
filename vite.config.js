f3e337286418f11100ec41e41fc705f21814385b
Usaid Ahmad <usaidmoiza@gmail.com>
2026-05-24 09:45:24 +0500

    configured the domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Demo/',
+  base: '/',
   plugins: [tailwindcss(),react()],
    server: {
         port: 3000, 
